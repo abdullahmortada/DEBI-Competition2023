25b5ab2504b79d234bce074c6d54c8b51adef10a
Abdullah Mortada <abdullahmortada@gmail.com>
2023-04-16 02:22:04 +0200

    added dockerfile

---
+++ ros-noetic-turtlebot/run.sh
@@ -0,0 +1,12 @@
+xhost local:root 
+XAUTH=/tmp/.docker.xauth 
+
+docker run -it \
+  --name=turt --env="DISPLAY=$DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
+  --env="XAUTHORITY=$XAUTH" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --volume="$XAUTH:$XAUTH" \
+  -v /home/abdullah/dev/debi:/root/catkin_ws/src/debi\
+  --net=host \
+  --privileged ros-noetic-turtlebot bash
