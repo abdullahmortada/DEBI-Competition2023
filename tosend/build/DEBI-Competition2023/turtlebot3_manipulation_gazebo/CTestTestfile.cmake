6a80593f0b970ba406d0204d9af50b6860fa5c57
Abdullah Mortada <abdullahmortada@gmail.com>
2023-04-17 03:18:58 +0200

    removed useless stuff

--- tosend/build/DEBI-Competition2023/turtlebot3_manipulation_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws/src/DEBI-Competition2023/turtlebot3_manipulation_gazebo
-# Build directory: /root/catkin_ws/build/DEBI-Competition2023/turtlebot3_manipulation_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- tosend/build/open_manipulator_dependencies/roboticsgroup_gazebo_plugins/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws/src/open_manipulator_dependencies/roboticsgroup_gazebo_plugins
-# Build directory: /root/catkin_ws/build/open_manipulator_dependencies/roboticsgroup_gazebo_plugins
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
