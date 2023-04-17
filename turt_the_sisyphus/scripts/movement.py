0a6175bd721b3a2f555ed1bb3c2aec8708915e31
Abdullah Mortada <abdullahmortada@gmail.com>
2023-04-17 03:18:23 +0200

    movement

---
+++ turt_the_sisyphus/scripts/movement.py
@@ -0,0 +1,123 @@
+import rospy
+from geometry_msgs.msg import Twist, Point
+from std_msgs.msg import Bool
+import tf
+from math import sqrt, pow, pi, atan2
+from tf.transformations import euler_from_quaternion
+import numpy as np
+
+
+def Callback(data):
+    def get_odom(tf_listener, odom_frame, base_frame):
+        try:
+            (trans, rot) = tf_listener.lookupTransform(odom_frame, base_frame, rospy.Time(0))
+            rotation = euler_from_quaternion(rot)
+
+        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+            rospy.loginfo("TF Exception")
+            return
+
+        return (Point(*trans), rotation[2])
+
+    move_cmd = Twist()
+    rate = rospy.Rate(10)
+    tf_listener = tf.TransformListener()
+    odom_frame = 'odom' 
+    is_ready.publish(Bool(False))
+
+    try:
+        tf_listener.waitForTransform(odom_frame, 'base_footprint', rospy.Time(), rospy.Duration(1.0))
+        base_frame = 'base_footprint'
+    except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+        try:
+            tf_listener.waitForTransform(odom_frame, 'base_link', rospy.Time(), rospy.Duration(1.0))
+            base_frame = 'base_link'
+        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+            rospy.loginfo("Cannot find transform between odom and base_link or base_footprint")
+            rospy.signal_shutdown("tf Exception")
+            return 
+
+    
+    (position, rotation) = get_odom(tf_listener, odom_frame, base_frame)
+    last_rotation = 0
+    linear_speed = 1
+    angular_speed = 1
+    (goal_x, goal_y, goal_z) = (data.x, data.y, data.z)
+    goal_z = np.deg2rad(goal_z)
+    goal_distance = sqrt(pow(goal_x - position.x, 2) + pow(goal_y - position.y, 2))
+    distance = goal_distance
+
+    while distance > 0.05:
+        (position, rotation) = get_odom(tf_listener, odom_frame, base_frame)
+        x_start = position.x
+        y_start = position.y
+        path_angle = atan2(goal_y - y_start, goal_x- x_start)
+
+        if path_angle < -pi/4 or path_angle > pi/4:
+            if goal_y < 0 and y_start < goal_y:
+                path_angle = -2*pi + path_angle
+            elif goal_y >= 0 and y_start > goal_y:
+                path_angle = 2*pi + path_angle
+        if last_rotation > pi-0.1 and rotation <= 0:
+            rotation = 2*pi + rotation
+        elif last_rotation < -pi+0.1 and rotation > 0:
+            rotation = -2*pi + rotation
+        move_cmd.angular.z = angular_speed * path_angle-rotation
+
+        distance = sqrt(pow((goal_x - x_start), 2) + pow((goal_y - y_start), 2))
+        move_cmd.linear.x = min(linear_speed * distance, 0.4)
+
+        if move_cmd.angular.z > 0:
+            move_cmd.angular.z = min(move_cmd.angular.z, 1.5)
+        else:
+            move_cmd.angular.z = max(move_cmd.angular.z, -1.5)
+
+        last_rotation = rotation
+        cmd_vel.publish(move_cmd)
+        rate.sleep()
+
+    (position, rotation) = get_odom(tf_listener, odom_frame, base_frame)
+
+    while abs(rotation - goal_z) > 0.05:
+        (position, rotation) = get_odom(tf_listener, odom_frame, base_frame)
+        if goal_z >= 0:
+            if rotation <= goal_z and rotation >= goal_z - pi:
+                move_cmd.linear.x = 0.00
+                move_cmd.angular.z = 0.5
+            else:
+                move_cmd.linear.x = 0.00
+                move_cmd.angular.z = -0.5
+        else:
+            if rotation <= goal_z + pi and rotation > goal_z:
+                move_cmd.linear.x = 0.00
+                move_cmd.angular.z = -0.5
+            else:
+                move_cmd.linear.x = 0.00
+                move_cmd.angular.z = 0.5
+        cmd_vel.publish(move_cmd)
+        rate.sleep()
+
+
+    rospy.loginfo("Stopping the robot...")
+    cmd_vel.publish(Twist())
+    is_ready.publish(Bool(True))
+    rospy.sleep(2)
+
+
+def shutdown():
+    global cmd_vel
+    cmd_vel.publish(Twist())
+    rospy.sleep(1)
+
+
+if __name__ == '__main__':
+    global cmd_vel, is_ready
+    rospy.init_node('movement', anonymous=False)
+    sub = rospy.Subscriber("/movement_queue", Point, Callback)
+
+    cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=5)
+    is_ready = rospy.Publisher("/is_ready", Bool, queue_size=5)
+
+    rospy.on_shutdown(shutdown)
+    rospy.spin()
+
---
+++ turt_the_sisyphus/scripts/pub.py
@@ -0,0 +1,36 @@
+import rospy 
+from geometry_msgs.msg import Point, Twist
+from std_msgs.msg import Bool
+
+move_list = [Point(1.1, 0, 0), 
+             Point(0, 0.96, 0), Point(1.1, 0.96, 1), 
+             Point(0, -0.92, 0), Point(1.1, -0.92, 0)]
+
+def reverse(data):
+    if data.data:
+        if len(move_list) % 2 == 0:
+            cmd = Twist()
+            cmd.linear.x = -0.2
+            cmd.angular.z = 0.8
+            cmd_vel.publish(cmd)
+            rospy.sleep(1)
+        if move_list:
+            pub.publish(move_list[0])
+    else:
+        move_list.pop(0)
+
+def shutdown():
+    global cmd_vel 
+    cmd_vel.publish(Twist())
+
+if __name__ == "__main__":
+    global cmd_vel
+    rospy.init_node("pub", anonymous=True)
+    pub = rospy.Publisher("/movement_queue", Point, queue_size=5)
+    cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=5)
+    is_ready = rospy.Subscriber("/is_ready", Bool, reverse)
+    rospy.on_shutdown(shutdown)
+    rospy.sleep(1)
+    pub.publish(move_list[0])
+    rospy.spin()
+
